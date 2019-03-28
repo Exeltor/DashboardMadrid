b7ff0de8208a61bfe57101929ec692e4a15fd389
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-03-28 12:53:19 +0100

    Delete mongo_insert_salidas.py

--- scrapers_python/mongo_insert_salidas.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-import urllib.parse
-from scraper_salidas_aeropuerto import vuelosFormateados
-
-
-client = pymongo.MongoClient("mongodb+srv://Master:" + urllib.parse.quote_plus('masterP@ss') + "@tpa-whplr.mongodb.net/test?retryWrites=true")
-db = client.TPA.salidasAeropuerto
-
-db.insert_many(vuelosFormateados)
