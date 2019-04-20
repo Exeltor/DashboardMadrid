ab4c6808ad91438ad130023b569ad8ad040bc133
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-04-20 13:58:16 +0200

    Solucionado error con en entrelazado de las estaciones y lineas

--- Metro/api_scraper_unifier.py
+++ Metro/api_scraper_unifier.py
@@ -10,7 +10,11 @@ for linea in lineas:
 	linea.update(_id = objId)
 
 	for estacion in estaciones:
-		if linea['nombre'].startswith(estacion['linea']):
+		if linea['nombre'] == estacion['linea']:
+			estacion.update(_idLinea = objId)
+		elif (linea['nombre'] == 'Línea 6 Circular') and (estacion['linea'] == 'Línea 6'):
+			estacion.update(_idLinea = objId)
+		elif (linea['nombre'] == 'Línea 12 MetroSur') and (estacion['linea'] == 'Línea 12'):
 			estacion.update(_idLinea = objId)
 
 client = pymongo.MongoClient("mongodb+srv://Master:" + urllib.parse.quote_plus('masterP@ss') + "@tpa-whplr.mongodb.net/test?retryWrites=true")
