377f413c3cb740e747b379c2cfdfe15133c8ecf9
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-05-08 14:38:23 +0200

    Cosas hechas
    -Aeropuerto
    -Tiempo
    -Medio del metro

--- Plantilla final/index.js
+++ Plantilla final/index.js
@@ -14,8 +14,36 @@ app.use(bodyParser.urlencoded({
 app.use(express.static('public'));
 
 //Dir principal
-app.get('/', function(req, res){
-  res.render('index', {par:'Init'});
+app.get('/', async function(req, res){
+  //Aplicable para una coleccion entera
+  MongoClient.connect(url, {useNewUrlParser:true}).then(async function (db) {
+    dbo = db.db('TPA')
+
+    //Lista general de insercion al index
+    var list = {};
+
+    //Llegadas/Salidast
+    list.flightDepJsons = await dbo.collection('salidasAeropuerto').find().toArray();
+    list.flightArrJsons = await dbo.collection('llegadasAeropuerto').find().toArray();
+
+
+    //Tiempo
+    weatherCallbacks = await dbo.collection('tiempoPorHora').find().sort({'dia/hora': 1}).toArray();
+
+    list.weatherIcon = weatherCallbacks[0].icono;
+    list.weatherTemp = weatherCallbacks[0].temp;
+    list.weatherDesc = weatherCallbacks[0].desc;
+    list.weatherHour = weatherCallbacks[0]['dia/hora'];
+
+    //Metro
+    metroLinesDb = await dbo.collection('lineasMetro').find().sort({'nombre': 1}).toArray();
+
+    list.metroLines = metroLinesDb;
+
+
+    //Render final de la pagina con los datos
+    res.render('index', list);
+  });
 });
 
 //Query de mongo que no se debe perder nunca jamas. Usar esta plantilla para renderizar todas las nuevas paginas que
@@ -24,7 +52,7 @@ app.get('/mongo-test', function(req, res){
   MongoClient.connect(url, {useNewUrlParser:true}).then(function (db) {
     dbo = db.db('TPA')
     dbo.collection("tiempoPorHora").findOne({}).then(function(data) {
-          res.render('index', {par:JSON.stringify(data)});
+          //res.render('index', {par:JSON.stringify(data)});
     }).catch(function (err) {//failure callback
          console.log(err)
     });
