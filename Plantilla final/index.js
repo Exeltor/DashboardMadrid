282fe652927d82bdb8c4d88b708d21d2951c16ea
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-05-08 22:08:18 +0200

    Haciendo conversion de moneda

--- Plantilla final/index.js
+++ Plantilla final/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://webReadOnly:readOnly@tpa-whplr.mongodb.net/test?retryWrites=true";
+var request = require('request')
 var bodyParser=require("body-parser"); 
+var request = require('sync-request');
 
 
 app.set('view engine', 'pug');
@@ -37,12 +39,17 @@ app.get('/', async function(req, res){
 
     //Metro Lineas
     metroLinesDb = await dbo.collection('lineasMetro').find().sort({'nombre': 1}).toArray();
-
     list.metroLines = metroLinesDb;
+
+    //Exchange Rates
+    var response = request('GET', 'https://api.exchangeratesapi.io/latest?base=USD');
+    var coinTypeBase= JSON.parse(response.getBody('utf8'));
+    list.coinTypes = Object.keys(coinTypeBase.rates);
 
 
     //Render final de la pagina con los datos
     res.render('index', list);
+    db.close();
   });
 });
 
@@ -57,6 +64,18 @@ app.get('/mongo-test', function(req, res){
          console.log(err)
     });
   })
+});
+
+
+//post conversion
+app.post('/convert', function(req, res){
+  console.log(req.body.currencySelection);
+  res.redirect('/');
+})
+
+app.post('/selectCurrency', function(req, res){
+  console.log('currency selected');
+  res.redirect('/');
 });
 
 var port = 8080;
